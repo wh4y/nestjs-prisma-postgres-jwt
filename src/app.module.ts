3b49c1a78b91b6acdac21bb7b4c6b9dcf4467f71
Anton Veselev <wh4y@mail.ru>
2022-03-24 11:08:27 +0300

    .env configuration integrated into root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from "@nestjs/common";
-import { UsersModule } from './users/users.module';
-import { DatabaseModule } from './database/database.module';
-import { AuthModule } from './auth/auth.module';
-import { TestJwtModule } from './testjwt/test-jwt.module';
-import { SharedModule } from './shared/shared.module';
+import { UsersModule } from "./users/users.module";
+import { DatabaseModule } from "./database/database.module";
+import { AuthModule } from "./auth/auth.module";
+import { TestJwtModule } from "./testjwt/test-jwt.module";
+import { SharedModule } from "./shared/shared.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule, TestJwtModule, SharedModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+    DatabaseModule,
+    AuthModule,
+    TestJwtModule,
+    SharedModule
+  ]
 })
-export class AppModule {}
+export class AppModule {
+}
